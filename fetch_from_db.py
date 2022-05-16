ed2bbfe3e7738771fdc1a802081b342ddf9a39d2
Mia Yi <mia_yi@fastmail.com>
2022-05-17 00:48:05 +0100

    connected to database and created databases

---
+++ fetch_from_db.py
@@ -0,0 +1,16 @@
+import pymysql
+import sqlalchemy
+import pandas as pd
+
+conn = sqlalchemy.create_engine('mysql+pymysql://root:123456@localhost:3366/mini_project')
+
+sql = '''
+select * from courier
+where courier_id = 1;
+'''
+
+df = pd.read_sql(sql, conn)
+print(df)
+a = pd.DataFrame.to_dict(df)
+print(a)
+
