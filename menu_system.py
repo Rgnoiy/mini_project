25e5b382e488dc4c9c5f25a404b173c3b9d108ed
Mia Yi <mia_yi@fastmail.com>
2022-05-16 01:07:40 +0100

    modified main meun to user input

--- menu_system.py
+++ menu_system.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
-product_list = pd.read_csv('products.csv').to_dict()
-main_menu_option = ["Please choose what you want to do next:\n"
-                    "0. SAVE & EXIT",
-                    "1. check product menu",
-                    "2. check courier options",
-                    "3. your order",
-                    ]
+product_list = pd.read_csv('products.csv')
+
+while True:
+    user_input = input(
+        "Please choose what you want to do next:\n0. SAVE & EXIT\n1. check product menu\n2. check courier options\n3. your order"
+    )
+    if user_input == "1":
+        print(product_list)
+    else:
+        break
