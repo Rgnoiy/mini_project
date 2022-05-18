2538835936f152a7ef59c592dcc40b85346ea999
Mia Yi <mia_yi@fastmail.com>
2022-05-18 01:15:27 +0100

    a bit progress for Tue

--- fetch_from_db.py
+++ fetch_from_db.py
@@ -1,16 +1,73 @@
 import pymysql
-import sqlalchemy
+from sqlalchemy import *
 import pandas as pd
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.mysql import INTEGER, FLOAT
 
-conn = sqlalchemy.create_engine('mysql+pymysql://root:123456@localhost:3366/mini_project')
+conn = create_engine('mysql+pymysql://root:123456@localhost:3366/mini_project')
+metadata = MetaData(conn)
+# conn_pysql = pymysql.connect(user='root', password='123456', host='localhost', database='mini_project', port=3366,
+#                              charset='utf8')
+# mycursor = conn_pysql.cursor()
+
+if __name__ == '__main__':
+    couriers = Table('couriers', metadata,
+                     Column('courier_id', Integer, nullable=False, primary_key=True, autoincrement=True),
+                     Column('courier_name', VARCHAR(45), nullable=False),
+                     Column('courier_phone', CHAR(20), nullable=False, unique=True))
+    orders = Table('orders', metadata,
+                   Column('order_id', Integer, nullable=False, primary_key=True, autoincrement=True),
+                   Column('customer_name', VARCHAR(45), nullable=False),
+                   Column('customer_address', VARCHAR(60), nullable=False),
+                   Column('customer_phone', VARCHAR(45), nullable=False, unique=True),
+                   Column('courier_id', INTEGER, nullable=False),
+                   Column('order_status', VARCHAR(20), nullable=False, server_default='unknown'))
+    products = Table('products', metadata,
+                     Column('product_id', Integer, primary_key=True, nullable=False, autoincrement=True),
+                     Column('product_name', VARCHAR(50), unique=True, nullable=False),
+                     Column('price', FLOAT(unsigned=True), nullable=False))
+    ordered_items = Table('ordered_items', metadata,
+                          Column('order_id', Integer, nullable=False),
+                          Column('ordered_items', Integer, nullable=False))
+    metadata.create_all()
+
+    _ = pd.DataFrame({"courier_id": [1, 2, 3], "courier_name": ['Mike', 'John', 'Mary'],
+                      "courier_phone": ['07264759682', '07294756384', '07593575834']})
+    _.to_sql('couriers', con=conn, if_exists='append', index=False)
+
+    _ = pd.DataFrame({"order_id": [1, 2, 3],
+                      "customer_name": ['Oli', 'Bala', 'Saira'],
+                      "customer_address": ['Flat 12, 56A Lant Street, London', '14 Howerd Way, London',
+                                           '6 Combwell Crescent, London'],
+                      "customer_phone": ['(020) 7317 2120', '(01689) 446622', '(01322) 276751'],
+                      "courier_id": [1, 3, 3],
+                      "order_status": ['preparing', 'preparing', 'preparing']})
+    _.to_sql('orders', con=conn, if_exists='append', index=False)
+
+    _ = pd.DataFrame({"order_id": [1, 1, 1, 2, 2, 3],
+                      "ordered_items": [4, 3, 5, 6, 7, 8]})
+    _.to_sql("ordered_items", con=conn, if_exists='append', index=False)
+
 
 sql = '''
-select * from courier
-where courier_id = 1;
+select ordered_items from mini_project.ordered_items
+left join mini_project.orders
+on ordered_items.order_id = orders.order_id
 '''
+# # def print_order_list
+#     """select * from order
+#        outer join ordered_items
+#        on ordered_items.order_id = order.order_id"""
+# # def print_courier_list
+#     """select * from courier"""
+# # def print_product_list
+#     """select * from mini_project.product"""
+# def ordered_items():
+#     """select ordered_items from mini_project.ordered_items
+#        left join mini_project.order
+#        on ordered_items.order_id = order.order_id"""
 
 df = pd.read_sql(sql, conn)
 print(df)
-a = pd.DataFrame.to_dict(df)
+a = pd.DataFrame.to_dict(df)        # fix the index
 print(a)
-
--- menu_system.py
+++ menu_system.py
@@ -1,38 +1,15 @@
 import pandas as pd
 import pymysql
 
-# product_list = pd.read_csv('products.csv')
+
 empty_product_list = []
 while True:
-    print("Main Menu\n0. SAVE CHANGES & EXIT\n1. product\n2. courier\n3. order")
+    print("Main Menu\n0. EXIT\n1. product\n2. courier\n3. order")
     user_input_for_main_menu = input("Please choose what's next: ")
     print("-" * 50)
 
     if user_input_for_main_menu == "0":
-        # SAVE products list to products.csv
-        # SAVE couriers list to couriers.csv
-        # SAVE orders list to order.csv
-        # EXIT app
-        if len(empty_product_list) != 0:
-            try:
-                product_list = pd.read_csv('products.csv')
-            except pd.errors.EmptyDataError:
-                df1 = pd.DataFrame.from_dict(empty_product_list[0])
-                df1.to_csv('products.csv', mode='w')
-                try:
-                    for a_product in empty_product_list[1:]:
-                        df1 = pd.DataFrame.from_dict(a_product)
-                        df1.to_csv('products.csv', mode='a', index=False, header=False)
-                except ValueError:
-                    pass
-            else:
-                for a_product in empty_product_list:
-                    df1 = pd.DataFrame.from_dict(a_product)
-                    df1.to_csv('products.csv', mode='a', index=False, header=False)
-            print("Product menu has been successfully updated!")
-            break
-        else:
-            print("Nothing has been changed. Exit successfully.")
+            print("Exit successfully.")
             break
 
 
@@ -49,7 +26,7 @@ while True:
             else:
                 print(empty_product_list)
                 print("-" * 50)
-        elif user_input_for_product == "2":
+        elif user_input_for_product == "2":     # FOR NEW PRODUCT
             print("You are adding new product to the menu.")
             product = input("product name: ").casefold()
             while True:
@@ -57,21 +34,93 @@ while True:
                     price = float(input(f"price for {product}: "))
                     break
                 except ValueError:
-                    print("Please type in valid price!")
+                    print("Please type a valid price!")
+            # TODO: insert inputs into db
             new_product_and_price = {
                 'product': f'{product}',
                 'price': f'{price}'
             }
-            print(new_product_and_price)
-            empty_product_list.append(new_product_and_price)
-            print(empty_product_list)
-            print("Your change is pending. Please go back to main menu and save it!")
+            # print(new_product_and_price)
+            # empty_product_list.append(new_product_and_price)
+            # print(empty_product_list)
+            # print("Your change is pending. Please go back to main menu and save it!")
             print("-" * 50)
-        elif user_input_for_product == "3":
-
+        elif user_input_for_product == "3":         # update
+            # TODO: 1. get all products from products table and print ALL product IDS
+            # TODO: 2. get user input for product ID/name/price
+            # TODO: 3. if any inputs are empty, do not update them, do what then..?
+            # TODO: 4. UPDATE properties for Products in product table
+        elif user_input_for_product == "4":     # DELETE product
+            # TODO: 1. get all products and print IDS from products table(duplicate->use def)
+            # TODO: 2. get user input for product ID
+            # TODO: 3. DELETE product in products table
         else:
             print("!!!Please choose a valid option!!!")
             print("-" * 50)
+
+    while user_input_for_main_menu == "2":
+        print("Courier Menu\n0. return to main menu\n1. check couriers list\n2. add new courier to list\n3. update existing courier\n4. delete courier")
+        user_input_for_courier = input("Please choose what's next: ")
+
+        if user_input_for_courier == "0":
+            break
+        elif user_input_for_courier == "1":
+            # TODO: print courier list
+        elif user_input_for_courier == "2":     # create new courier
+            # TODO: get user input for courier name/courier phone number
+            # TODO: INSERT courier into couriers table
+        elif user_input_for_courier == "3":     # update existing courier
+            # TODO: get all couriers from couriers table and print couriers with their IDs
+            # TODO: get user input for courier ID/name/phone number
+            # TODO: if input is empty, do not update its respective table property
+            # TODO: UPDATE properties for courier in courier table
+        elif user_input_for_courier == "4":        # DELETE existing courier
+            # TODO: get all couriers from couriers table and print couriers with their IDs(duplicate)
+            # TODO: get user input for courier ID
+            # TODO: delete it from table
+        else:
+            print("!!!Please choose a valid option!!!")
+            print("-" * 50)
+
+    while user_input_for_main_menu == "3":
+        print("Order Menu\n0. return to main menu\n1. check order list\n2. add new order to list\n3. update existing product\n4. delete product")
+        user_input_for_order = input("Please choose what's next: ")
+        if user_input_for_order == "0":
+            break
+        elif user_input_for_order == "1":
+            # TODO: print order dict
+        elif user_input_for_order == "2":
+            # TODO: user input for name/address(sepreate lines)/phone number
+            # TODO: print products list with its index values
+            # TODO: get user input for comma-separated list of product index values
+                # TODO: convert user input to list of integers
+            # TODO: PRINT couriers list with index value for each courier
+            # TODO: get user input for courier index to select courier
+            # TODO: SET order status to be 'PREPARING'(trigger)
+            # TODO: CREATE AND display order as dict with the information provided above
+            # TODO: append order to orders list
+        elif user_input_for_order == "3":       # update status
+            # TODO: PRINT orders list with order_id
+            # TODO: GET user input for order_id
+            # TODO: PRINT order status list with index values?
+            # TODO: Get user input for order status index value?
+            # TODO: UPDATE status for order
+        elif user_input_for_order == "4":       # update existing order
+            # TODO: print orders list with order_id(duplicate)
+            # TODO: FOR EACH key-value pair in selected order:
+                        #GET user input for updated property
+                        #IF user input is blank:
+                            #do not update this property
+                        #ELSE:
+                            #update the property value with user input
+        elif user_input_for_order == "5":
+            # TODO: print orders list
+            # TODO: get user input for order_id
+            # TODO: DELETE that order
+        else:
+            print("!!!Please choose a valid option!!!")
+            print("-" * 50)
+            
 
     if user_input_for_main_menu != "0" and user_input_for_main_menu != "1" and user_input_for_main_menu != "2" and user_input_for_main_menu != "3":
         print("!!!Please choose a valid option!!!")
